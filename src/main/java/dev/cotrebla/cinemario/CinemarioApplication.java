d0a38817ccd8530ec8d4d33dbbb3027b3aa53eeb
Alberto Cuchillo <cotrebla@gmail.com>
2026-03-05 00:50:31 +0100

    Primer commit de Sb_cine

---
+++ src/main/java/dev/cotrebla/cinemario/CinemarioApplication.java
@@ -0,0 +1,13 @@
+package dev.cotrebla.cinemario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cotrebla/cinemario/CinemarioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cotrebla.cinemario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
